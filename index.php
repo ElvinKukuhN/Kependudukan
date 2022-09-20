713ac6122c960364c40794cc6764229b76306a82
Elvin Kukuh Nurdiyansah <elvinkukuh20@student.uns.ac.id>
2022-09-20 21:24:20 +0700

    Update index.php

--- index.php
+++ index.php
@@ -289,7 +289,7 @@ if (isset($_POST['Update'])) {
             <img src="dist/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image">
           </div>
           <div class="info">
-            <a href="#" class="d-block">Mohammad Jose Rizal</a>
+            <a href="#" class="d-block">Elvin Kukuh N</a>
           </div>
         </div>
 
@@ -308,8 +308,7 @@ if (isset($_POST['Update'])) {
         <!-- Sidebar Menu -->
         <nav class="mt-2">
           <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
-            <!-- Add icons to the links using the .nav-icon class
-               with font-awesome or any other icon font library -->
+            <!-- Add icons to the links using the .nav-icon class with font-awesome or any other icon font library -->
             <li class="nav-item">
               <a href="#" class="nav-link">
                 <i class="nav-icon fas fa-tachometer-alt"></i>
